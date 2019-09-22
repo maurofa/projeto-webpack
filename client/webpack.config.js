58374829b7488baf94421a72d121e9d95deec081
Mauro Farias Brito <mauro.brito@mpdft.mp.br>
2019-09-21 23:24:59 -0300

    configurando uma url diferente de produção.

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -36,7 +36,10 @@ plugins.push(
     )
 ); 
 
+let SERVICE_URL = JSON.stringify('http://localhost:3000');
+
 if(process.env.NODE_ENV == 'production') {
+    SERVICE_URL = JSON.stringify('http://endereco-da-sua-api');
     plugins.push(new webpack.optimize.ModuleConcatenationPlugin());
     plugins.push(new babiliPlugin());
 
@@ -50,6 +53,8 @@ if(process.env.NODE_ENV == 'production') {
         canPrint: true
      }));
 }
+
+plugins.push(new webpack.DefinePlugin({ SERVICE_URL }));
 
 module.exports = {
     entry: {
